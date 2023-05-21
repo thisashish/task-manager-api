d6716d578f0b302599a1f50e2de367c3ff9558ed
Ashish Vishwakarma <75529301+thisashish@users.noreply.github.com>
2023-05-21 22:37:58 +0530

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/user')
 
+// middleware
+
 const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '')
